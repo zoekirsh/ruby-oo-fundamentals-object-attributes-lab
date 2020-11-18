a4be631d109b1c126c0aa6dba3d7fb3ffa4cc3b8
Zoe Kirsh <zoekirsh@gmail.com>
2020-11-17 19:28:22 -0800

    done

---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+
+class Person
+
+    def initialize
+        @name
+        @job
+    end
+
+    def name= (name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def job= (job)
+        @job = job
+    end
+    
+    def job
+        @job    
+    end
+
+end
